f2851bf220b35bebc52afeb86e7ba9461d4cacb2
Peyman-98 <ds_worm@yahoo.com>
2015-10-07 14:16:07 +0330

    The End

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-window.addEventListener("load",function(){
-	var load_screen=document.getElementById("load_screen");
-	document.body.removeChild(load_screen);
-});
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
